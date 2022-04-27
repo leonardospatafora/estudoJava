1de8f8a985eaf8de3a0e6704254b659d4993d39e
Leonardo Spatafora <leonardo_spatafora@outlook.com>
2022-04-27 15:11:37 -0300

    Commit

---
+++ Testes/src/TestePorcentagem.java
@@ -0,0 +1,12 @@
+
+public class TestePorcentagem {
+
+	public static void main(String[] args) {
+		double valor = 200.0;
+		
+		double valor2 = valor + 50;
+		
+		System.out.println(valor2);
+	} 
+
+}
