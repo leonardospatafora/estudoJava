4e0a95affd708de783c1fc3864b2e607de0ee60b
leonardo.spatafora <leonardo.spatafora@socin.com.br>
2023-01-12 17:47:45 -0300

    atualização branch

--- Testes/src/TestesAleatorios.java
+++ Testes/src/TestesAleatorios.java
@@ -41,13 +41,24 @@ public class TestesAleatorios {
 //			System.out.println("andou aqui");
 //		}
 		
-		BigDecimal valor = new BigDecimal("100000");
-		BigDecimal valorComparador = new BigDecimal("10000");
-		if (valor.compareTo(valorComparador) > 1)  {
-			throw new IllegalAccessException("Funcionario com salario maior que dez mil não recebe bonus");
-		}
+//		BigDecimal valor = new BigDecimal("100000");
+//		BigDecimal valorComparador = new BigDecimal("10000");
+//		if (valor.compareTo(valorComparador) > 1)  {
+//			throw new IllegalAccessException("Funcionario com salario maior que dez mil não recebe bonus");
+//		}
 		
-		
-		
+	    int number=10;   
+	       // Expressão Switch   
+	       switch(number){   
+	          //Declarações case   
+	          case 10: System.out.println("10");   
+	             break;   
+	          case 20: System.out.println("20");   
+	             break;   
+	          case 30: System.out.println("30");   
+	             break;   
+	           //Declaração default case   
+	           default:System.out.println("Not in 10, 20 or 30");   
+	      }   
 }
 }
